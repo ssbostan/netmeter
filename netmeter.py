c97b3119cb77e7e0b873ea6dd29a49ae8a2db79b
Saeid Bostandoust <ssbostan@gmail.com>
2020-01-23 12:41:38 +0330

    add netmeter functions

---
+++ netmeter.py
@@ -0,0 +1,20 @@
+from time import sleep
+
+def getNetInfo(iface, ctype):
+    with open(f"/sys/class/net/{iface}/statistics/{ctype}_bytes", "r") as mfile:
+        cbytes=mfile.read()
+    with open(f"/sys/class/net/{iface}/statistics/{ctype}_packets", "r") as mfile:
+        cpackets=mfile.read()
+    return (int(cbytes), int(cpackets))
+    
+def getRX(iface):
+    T1=getNetInfo(iface, "rx")
+    sleep(1)
+    T2=getNetInfo(iface, "rx")
+    return (T2[0]-T1[0], T2[1]-T1[1])
+    
+def getTX(iface):
+    T1=getNetInfo(iface, "tx")
+    sleep(1)
+    T2=getNetInfo(iface, "tx")
+    return (T2[0]-T1[0], T2[1]-T1[1])
